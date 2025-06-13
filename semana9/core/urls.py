350ecde3a4c5e061d8c3e47cfa171f6468940e0c
dbernardos <davi.bernardo@ifsc.edu.br>
2025-06-13 18:14:59 -0300

    Delete

--- semana9/core/urls.py
+++ semana9/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, livro
 
 urlpatterns = [
     path('', index),
+    path('Livro/<int:pk>', livro, name='Livro'),
+    path('del_livro/<int:pk>', livro, name='del_livro')
 ]
--- semana9/core/views.py
+++ semana9/core/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Livro
 
 def index(request):
-    return render(request, 'index.html')
+    livros = Livro.objects.all()
+    context = {
+        'livro': livros
+    }
+    return render(request, 'index.html', context)
+
+def livro(request, pk):
+    livro = Livro.objects.get(id=pk)
+    context = {
+        'livro': livro
+    }
+    return render(request, 'livro.html', context)
+
+def del_livro(request, pk):
+    livro = Livro.objects.get(id=pk)
+    print(f'>>>>>>>>>>> {livro}')
+    #if request.method == 'POST':
+    livro.delete()
+    return redirect('index')
+    
