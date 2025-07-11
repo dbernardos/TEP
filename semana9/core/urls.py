2b4f17050b77f9d7e7de3e821c84d804b5c416f0
dbernardos <121075844+dbernardos@users.noreply.github.com>
2025-07-11 17:00:01 -0300

    sistema de login

--- semana9/core/urls.py
+++ semana9/core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('del_livro/<int:pk>', views.del_livro, name='deletar_livro_url'),
     path('upg_livro/<int:pk>', views.upg_livro, name='atualizar_livro_url'),
     path('cad_livro', views.cadastrar_livro, name='cadastrar_livro_url'),
+    path('cad_user', views.cad_user, name='cad_user_url'),
+    path('entrar', views.entrar, name='entrar_url'),
+    path('sair', views.sair, name='sair_url'),
+    path('graf', views.graf, name='graf_url'),
 ]
